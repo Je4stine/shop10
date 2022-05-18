35cb36b7bb6b0ce8f45fdc4999f8643b45a2382d
Je4stine <je4stine@gmail.com>
2022-05-18 21:18:51 +0300

    One time

---
+++ build/precache-manifest.c2de42abb425de391615a327dd3100b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cfc2c12a483049c2e5bef627c23b7e",
+    "url": "/shopping-cart-react/index.html"
+  },
+  {
+    "revision": "47c66f67c9626ee03b0a",
+    "url": "/shopping-cart-react/static/css/main.eafeacb5.chunk.css"
+  },
+  {
+    "revision": "8f0aac18e45bf5525a34",
+    "url": "/shopping-cart-react/static/js/2.6ed1760b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart-react/static/js/2.6ed1760b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c66f67c9626ee03b0a",
+    "url": "/shopping-cart-react/static/js/main.d2cba7d5.chunk.js"
+  },
+  {
+    "revision": "fc53af84ab093c63cb89",
+    "url": "/shopping-cart-react/static/js/runtime-main.c367a05c.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/shopping-cart-react/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/shopping-cart-react/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/shopping-cart-react/static/media/times-solid.2c5552a1.svg"
+  }
+]);
